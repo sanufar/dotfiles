351da81f27ab02755a698b106ae03c412e49ebda
Farhan <141865586+sanufar@users.noreply.github.com>
2025-03-17 23:32:40 -0400

    added code to path, nvm plugin, fixed formatting for nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
 	set -gx EDITOR nvim
-	set -g fish_greeting
+	#function fish_prompt
+	#	echo [$(date +%T)] $USER::(prompt_pwd --dir-length 3 --full-length-dirs 1)
+	#end
 end
+
 
 if test -d /home/linuxbrew/.linuxbrew # Linux
 	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
@@ -12,6 +15,12 @@ else if test -d /opt/homebrew # MacOS
 	set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
 	set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
 end
+
+function code
+  set location "$PWD/$argv"
+  open -n -b "com.microsoft.VSCode" --args $location
+end
+
 fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
 ! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
 ! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH;
@@ -23,4 +32,18 @@ fish_add_path -gP /usr/local/texlive/2024/bin/universal-darwin
 
 set fzf_fd_opts --hidden --max-depth 5
 
+# Base16 Shell
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/sanufar/miniconda/bin/conda
+    eval /Users/sanufar/miniconda/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/sanufar/miniconda/etc/fish/conf.d/conda.fish"
+        . "/Users/sanufar/miniconda/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/sanufar/miniconda/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
 
